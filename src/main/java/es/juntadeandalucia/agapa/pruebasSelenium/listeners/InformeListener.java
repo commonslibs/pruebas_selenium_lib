2d7039ed9a9e4745960cfc77a0f168b13bb63c5d
Luis Escamilla Martín <luis.escamilla@juntadeandalucia.es>
2024-06-24 07:49:47 +0200

    SSI_Tarea #96276: Imagen basada en selenium/node-chrome

--- src/main/java/es/juntadeandalucia/agapa/pruebasSelenium/listeners/InformeListener.java
+++ src/main/java/es/juntadeandalucia/agapa/pruebasSelenium/listeners/InformeListener.java
@@ -1,5 +1,10 @@
 package es.juntadeandalucia.agapa.pruebasSelenium.listeners;
 
+import static java.util.stream.Collectors.toList;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import es.juntadeandalucia.agapa.pruebasSelenium.utilidades.VariablesGlobalesTest;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -14,33 +19,23 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.IReporter;
 import org.testng.ISuite;
 import org.testng.ISuiteResult;
 import org.testng.ITestContext;
-import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
-import es.juntadeandalucia.agapa.pruebasSelenium.suite.TestSeleniumAbstracto;
-import es.juntadeandalucia.agapa.pruebasSelenium.utilidades.VariablesGlobalesTest;
-import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 public class InformeListener implements IReporter {
    private static final Logger LOGGER       = LoggerFactory.getLogger(InformeListener.class);
 
-   private static final String ROW_TEMPLATE =
-         "<tr class=\"%s\"><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>";
+   private static final String ROW_TEMPLATE = "<tr class=\"%s\"><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>";
 
    @Override
    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
@@ -48,77 +43,42 @@ public class InformeListener implements IReporter {
 
       final String body = suites.stream().flatMap(this.suiteToResults()).collect(Collectors.joining());
 
-      this.saveReportTemplate(outputDirectory,
-            reportTemplate.replaceFirst("</tbody>", String.format("%s</tbody>", body)));
+      this.saveReportTemplate(outputDirectory, reportTemplate.replaceFirst("</tbody>", String.format("%s</tbody>", body)));
 
       this.mergearReports(suites);
    }
 
    private void mergearReports(List<ISuite> suites) {
-      ExtentSparkReporter sparkMergeado =
-            new ExtentSparkReporter(VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + "PPI.html");
+      ExtentSparkReporter sparkMergeado = new ExtentSparkReporter(VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + "PPI.html");
       ExtentReports reportMergeado = new ExtentReports();
-      reportMergeado.attachReporter(sparkMergeado);
 
       Collections.sort(suites, new ComparadorISuite());
       for (ISuite suite : suites) {
          try {
-            // Agregar al Report PPI por suite.
-            String nombre = suite.getName();
-            String directorioOrigenJson =
-                  VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + nombre + File.separator;
-            String ficheroJson = directorioOrigenJson + nombre + "-Extendido.json";
-            if (TestSeleniumAbstracto.validarFichero(ficheroJson)) {
-               // Si existe el Extendido por SUITE. Se agrega al report.
-               this.agregarAlReport(reportMergeado, directorioOrigenJson, ficheroJson);
-            }
+            String directorioOrigenJson = VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + suite.getName() + File.separator;
+            String ficheroJson = directorioOrigenJson + File.separator + suite.getName() + "-Extendido.json";
+            reportMergeado.createDomainFromJsonArchive(ficheroJson);
 
-            // Agregar al Report PPI por test.
-            for (ITestNGMethod iTest : suite.getAllMethods()) {
-               nombre = TestSeleniumAbstracto.formatearNombreFichero(iTest.getXmlTest().getName());
-               directorioOrigenJson =
-                     VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + nombre + File.separator;
-               ficheroJson = directorioOrigenJson + nombre + "-Extendido.json";
-               // Si existe el Extendido por TEST. Se agrega al report.
-
-               if (TestSeleniumAbstracto.validarFichero(ficheroJson)) {
-                  this.agregarAlReport(reportMergeado, directorioOrigenJson, ficheroJson);
-               }
-               else {
-                  InformeListener.log.error("No existe: " + ficheroJson);
-               }
+            // Copiar las capturas de pantallas de error
+            File sourceDirectory = new File(directorioOrigenJson + VariablesGlobalesTest.DIRECTORIO_CAPTURAS);
+            if (sourceDirectory.exists()) {
+               File destinationDirectory =
+                     new File(VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + VariablesGlobalesTest.DIRECTORIO_CAPTURAS);
+               FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
             }
          }
          catch (Exception e) {
-            InformeListener.log.error(e.getLocalizedMessage());
+            log.error(e.getLocalizedMessage());
          }
       }
-
+      reportMergeado.attachReporter(sparkMergeado);
       reportMergeado.flush();
-   }
-
-   private void agregarAlReport(ExtentReports reportMergeado, String directorioOrigenJson, String ficheroJson) {
-      try {
-         InformeListener.log.debug("Agregando al PPI: " + ficheroJson);
-         reportMergeado.createDomainFromJsonArchive(ficheroJson);
-         // Copiar las capturas de pantallas de error
-         File sourceDirectory = new File(directorioOrigenJson + VariablesGlobalesTest.DIRECTORIO_CAPTURAS);
-         if (sourceDirectory.exists()) {
-            File destinationDirectory = new File(
-                  VariablesGlobalesTest.DIRECTORIO_TARGET_SUREFIRE_REPORTS + VariablesGlobalesTest.DIRECTORIO_CAPTURAS);
-            FileUtils.copyDirectory(sourceDirectory, destinationDirectory);
-         }
-      }
-      catch (IOException e) {
-         InformeListener.log.error("Error al agregar informacion al PPI final. ", e);
-      }
-
    }
 
    private static class ComparadorISuite implements java.util.Comparator<ISuite> {
       @Override
       public int compare(ISuite a, ISuite b) {
-         return StringUtils.compare(a.getName(), b.getName());
+         return a.getName().compareTo(b.getName());
       }
    }
 
@@ -142,22 +102,17 @@ public class InformeListener implements IReporter {
    }
 
    private List<String> generateReportRows(String testName, String suiteName, Set<ITestResult> allTestResults) {
-      return allTestResults.stream().map(this.testResultToResultRow(testName, suiteName)).collect(Collectors.toList());
+      return allTestResults.stream().map(this.testResultToResultRow(testName, suiteName)).collect(toList());
    }
 
    private Function<ITestResult, String> testResultToResultRow(String testName, String suiteName) {
-      return testResult -> {
-         return switch (testResult.getStatus()) {
-            case ITestResult.FAILURE -> String.format(InformeListener.ROW_TEMPLATE, "danger", suiteName, testName,
-                  testResult.getName(), "FAILED", "NA");
-            case ITestResult.SUCCESS -> String.format(InformeListener.ROW_TEMPLATE, "success", suiteName, testName,
-                  testResult.getName(), "PASSED",
-                  String.valueOf(testResult.getEndMillis() - testResult.getStartMillis()));
-            case ITestResult.SKIP -> String.format(InformeListener.ROW_TEMPLATE, "warning", suiteName, testName,
-                  testResult.getName(), "SKIPPED", "NA");
-            default -> "";
-         };
-      };
+      return testResult -> (switch (testResult.getStatus()) {
+         case ITestResult.FAILURE -> String.format(ROW_TEMPLATE, "danger", suiteName, testName, testResult.getName(), "FAILED", "NA");
+         case ITestResult.SUCCESS -> String.format(ROW_TEMPLATE, "success", suiteName, testName, testResult.getName(), "PASSED",
+               String.valueOf(testResult.getEndMillis() - testResult.getStartMillis()));
+         case ITestResult.SKIP -> String.format(ROW_TEMPLATE, "warning", suiteName, testName, testResult.getName(), "SKIPPED", "NA");
+         default -> "";
+      });
    }
 
    private String initReportTemplate() {
@@ -168,7 +123,7 @@ public class InformeListener implements IReporter {
          template = new String(reportTemplate, "UTF-8");
       }
       catch (IOException e) {
-         InformeListener.LOGGER.error("Problem initializing template", e);
+         LOGGER.error("Problem initializing template", e);
       }
       return template;
    }
@@ -176,14 +131,13 @@ public class InformeListener implements IReporter {
    private void saveReportTemplate(String outputDirectory, String reportTemplate) {
       new File(outputDirectory).mkdirs();
       try {
-         PrintWriter reportWriter =
-               new PrintWriter(new BufferedWriter(new FileWriter(new File(outputDirectory, "my-report.html"))));
+         PrintWriter reportWriter = new PrintWriter(new BufferedWriter(new FileWriter(new File(outputDirectory, "my-report.html"))));
          reportWriter.println(reportTemplate);
          reportWriter.flush();
          reportWriter.close();
       }
       catch (IOException e) {
-         InformeListener.LOGGER.error("Problem saving template", e);
+         LOGGER.error("Problem saving template", e);
       }
    }
 }
